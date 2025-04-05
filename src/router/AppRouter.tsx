dbdb64f93c370e2eaad05802fe72e9ab2df764fe
Andrés Salgado <andressalgado41@gmail.com>
2025-04-04 19:51:21 -0500

    Remove line

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Route, Routes } from 'react-router';
 import { ThreeMTPage } from '../pages/ThreeMTPage';
 import { WorkShopCityPage } from '../pages/WorkShopCityPage';
