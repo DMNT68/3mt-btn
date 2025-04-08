23b66152bdd0564d4f09a3afaf614d7958ddf0a6
Andrés Salgado <andressalgado41@gmail.com>
2025-04-08 16:12:45 -0500

    Add new state isLoading

--- src/hooks/useCall.ts
+++ src/hooks/useCall.ts
@@ -7,14 +7,15 @@ const retellWebClient = new RetellWebClient();
 
 export const useCall = (agentId: string, client: ClientAiConnect) => {
 	const [isCalling, setIsCalling] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
 		retellWebClient.on('call_started', () => {
-			console.log('call started');
+			// console.log('call started');
 		});
 
 		retellWebClient.on('call_ended', () => {
-			console.log('call ended');
+			// console.log('call ended');
 			setIsCalling(false);
 		});
 
@@ -30,13 +31,15 @@ export const useCall = (agentId: string, client: ClientAiConnect) => {
 		if (isCalling) {
 			retellWebClient.stopCall();
 		} else {
+			setIsLoading(true);
 			const { data } = await registerCall(agentId, client);
 			if (data.access_token) {
 				await retellWebClient.startCall({ accessToken: data.access_token });
 				setIsCalling(true);
 			}
+			setIsLoading(false);
 		}
 	};
 
-	return { isCalling, toggleCall };
+	return { isCalling, toggleCall, isLoading };
 };
