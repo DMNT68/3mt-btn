5b5d02e1190b29c0d506d1328b75da8fa9a22e1c
Andrés Salgado <andressalgado41@gmail.com>
2025-04-08 16:10:32 -0500

    Remove en refactor code

--- src/services/callService.ts
+++ src/services/callService.ts
@@ -8,7 +8,10 @@ const urlBase = 'https://aiconnect.flec-ec.com';
  * @param agentId ID del agente con el que se quiere iniciar la llamada
  * @returns Información de la llamada registrada
  */
-export async function registerCall(agentId: string, client: ClientAiConnect): Promise<RegisterCallResponse> {
+export async function registerCall(
+	agentId: string,
+	client: ClientAiConnect
+): Promise<RegisterCallResponse> {
 	try {
 		const response = await fetch(`${urlBase}/api/aiconnect/${client}`, {
 			method: 'POST',
@@ -23,7 +26,7 @@ export async function registerCall(agentId: string, client: ClientAiConnect): Pr
 		}
 
 		const data: RegisterCallResponse = await response.json();
-		console.log('[registerCall] ✅ Call registered:', data);
+		// console.log('[registerCall] ✅ Call registered:', data);
 		return data;
 	} catch (err) {
 		console.error('[registerCall] ❌ Error:', err);
