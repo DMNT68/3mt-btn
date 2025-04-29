16169b74b37327e12817d4e4521b50dbb3f1741a
Andrés Salgado <andressalgado41@gmail.com>
2025-04-29 12:44:59 -0500

    Add route sellmycar

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router';
 import { ThreeMTPage } from '../pages/ThreeMTPage';
 import { WorkShopCityPage } from '../pages/WorkShopCityPage';
+import { SellMyCarPage } from '../pages/SellMyCarPage';
 
 export const AppRouter = () => {
 	return (
 		<Routes>
 			<Route index path="3mt" element={<ThreeMTPage />} />
 			<Route path="workshopcity" element={<WorkShopCityPage />} />
+			<Route path="sellMyCar" element={<SellMyCarPage />} />
 		</Routes>
 	);
 };
