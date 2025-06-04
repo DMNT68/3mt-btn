f4bc8536ef40f779e17693dbd18e8df268316f86
Andrés Salgado <andressalgado41@gmail.com>
2025-06-04 17:15:15 -0500

    Add page button

---
+++ src/pages/InmobiliariaJorge.tsx
@@ -0,0 +1,24 @@
+// import CallButton from '../components/CallButton';
+import { useCall } from '../hooks/useCall';
+import '../assets/styles.css';
+import { ClientAiConnect } from '../types';
+// import LiquidButton from '../components/LiquidButton/LiquidButton';
+import CallButton from '../components/CallButton';
+
+const agentId = 'agent_90fa979c17c710c8479a772147';
+const client: ClientAiConnect = 'aiconnect';
+
+export const InmobiliariaJorge = () => {
+	const { isCalling, toggleCall, isLoading } = useCall(agentId, client);
+
+	return (
+		<div className="app-container">
+			<CallButton
+				className={isCalling ? 'btn-stopCall threemt' : 'btn-startCall-3mt'}
+				isCalling={isCalling}
+				label={isLoading ? 'Lamando...' : isCalling ? 'Detener llamada' : 'Pregunta a un experto'}
+				onClick={isLoading ? () => {} : toggleCall}
+			/>
+		</div>
+	);
+};
